cb4193eb8c1a4715fdbbd2ecd5640ced041fda2d
koratoo <lears98@naver.com>
2023-09-04 10:27:02 +0900

    2... 매핑,PathVariable

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -0,0 +1,37 @@
+package hello.springmvc.basic.requestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping({"/hello-basic"})
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping(value="/mapping-get-v1")
+    public String mappingGetV1(){
+        log.info("mapping-get-v1 test");
+        return "ok";
+    }
+
+    @GetMapping(value="/mapping-get-v2")
+    public String mappingGetV2(){
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info("mappingPath userId = {}",data);
+        return "ok";
+    }
+}
